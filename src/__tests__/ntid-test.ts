0c73f71791bfcaac13a391c3815276d3830268e7
Will Schurman <wschurman@gmail.com>
2025-08-07 13:05:07 -0700

    chore: update dependencies and add lint

--- src/__tests__/ntid-test.ts
+++ src/__tests__/ntid-test.ts
@@ -1,41 +1,43 @@
-import ntid from '../ntid';
+import { it, expect } from '@jest/globals';
+
+import { getTypeFromId, makeCompoundId, makeId, makeSymmetricId } from '../ntid';
 
 it('makes IDs with the given type', () => {
-  let id = ntid.makeId('test');
+  const id = makeId('test');
   expect(id).toMatch(/^test\[/);
 });
 
 it('makes IDs with 22 random characters', () => {
-  let type = 'test';
-  let id = ntid.makeId(type);
+  const type = 'test';
+  const id = makeId(type);
   expect(id.length).toBe(type.length + 2 + 22);
 });
 
 it('makes compound IDs', () => {
-  let id1 = ntid.makeId('type1');
-  let id2 = ntid.makeId('type2');
+  const id1 = makeId('type1');
+  const id2 = makeId('type2');
 
-  let compoundId = ntid.makeCompoundId('compound', [id1, id2]);
+  const compoundId = makeCompoundId('compound', [id1, id2]);
   expect(compoundId).toMatch(/^compound\[/);
 });
 
 it('makes symmetric compound IDs', () => {
-  let id1 = ntid.makeId('type1');
-  let id2 = ntid.makeId('type2');
+  const id1 = makeId('type1');
+  const id2 = makeId('type2');
 
-  let symmetricId1 = ntid.makeSymmetricId('sym', [id1, id2]);
-  let symmetricId2 = ntid.makeSymmetricId('sym', [id2, id1]);
+  const symmetricId1 = makeSymmetricId('sym', [id1, id2]);
+  const symmetricId2 = makeSymmetricId('sym', [id2, id1]);
   expect(symmetricId1).toMatch(/^sym\[/);
   expect(symmetricId1).toBe(symmetricId2);
 });
 
 it('extracts the types of ids', () => {
-  let type = 'test';
-  let compoundType = 'compound';
+  const type = 'test';
+  const compoundType = 'compound';
 
-  let id = ntid.makeId(type);
-  expect(ntid.getTypeFromId(id)).toBe(type);
+  const id = makeId(type);
+  expect(getTypeFromId(id)).toBe(type);
 
-  let compoundId = ntid.makeCompoundId(compoundType, [id, id]);
-  expect(ntid.getTypeFromId(compoundId)).toBe(compoundType);
+  const compoundId = makeCompoundId(compoundType, [id, id]);
+  expect(getTypeFromId(compoundId)).toBe(compoundType);
 });
